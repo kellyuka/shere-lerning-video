81e756381ea3c527898d81010947b3938b8c4531
kellyuka <yukamohri0719@yahoo.co.jp>
2022-04-29 00:14:49 +0900

    FIX:enviroment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,7 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 
+
 environment.plugins.prepend(
      'VueLoaderPlugin',
      new VueLoaderPlugin()
@@ -20,6 +21,19 @@ environment.plugins.prepend(
         __VUE_OPTIONS_API__: true,
         __VUE_PROD_DEVTOOLS__: true
     })
+)
+const webpack = require('webpack')
+const dotenv = require('dotenv')
+const dotenvFiles = [
+    '.env'
+]
+dotenvFiles.forEach((dotenvFile) => {
+    dotenv.config({ path: dotenvFile, silent: true })
+})
+environment.plugins.prepend('Environment',
+    new webpack.EnvironmentPlugin(
+        JSON.parse(JSON.stringify(process.env))
+    )
 )
 
 module.exports = environment
