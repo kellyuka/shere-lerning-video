2de44b684b7a531dd60cf7c6e37ecb625287542a
kellyuka <yukamohri0719@yahoo.co.jp>
2022-04-30 09:46:56 +0900

    ADD:tagモデル追記

--- app/models/list_tag.rb
+++ app/models/list_tag.rb
@@ -1,4 +1,5 @@
 class ListTag < ApplicationRecord
   belongs_to :list
   belongs_to :tag
+
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  has_many :list_tags
+  has_many :lists, through: :list_tags
 end
