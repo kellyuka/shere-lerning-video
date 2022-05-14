31b5612429d55bfb6234b16927455ccbf444b324
kellyuka <yukamohri0719@yahoo.co.jp>
2022-05-14 15:57:09 +0900

    FIX:amazons3をproductionsで使用_2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,8 +32,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-  # amazons3に変更
-  config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
