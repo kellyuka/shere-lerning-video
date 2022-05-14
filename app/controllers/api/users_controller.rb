d4de2d4de5ea2057c930cfa2176cb87996947b00
kellyuka <yukamohri0719@yahoo.co.jp>
2022-05-14 15:15:08 +0900

    FIX:profileがnilでバリデーションエラーが起きる

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -23,6 +23,6 @@ class Api::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name, :channelid, :avatar)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :channelid, :avatar, :profile)
   end
 end
