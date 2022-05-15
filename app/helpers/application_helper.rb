03213a882d4f50a59ba435eebb364510e536e0e0
kellyuka <yukamohri0719@yahoo.co.jp>
2022-05-15 15:22:08 +0900

    ADD:ogp作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      title: 'ShareTechTube',
+      description: 'ShareTechTubeはプログラミングに特化したyoutube共有プラットフォームです',
+      keywords: 'youtube,プログラミング',
+      noindex: !Rails.env.production?, 
+      canonical: request.original_url,
+      charset: 'UTF-8',
+      og: {
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        site_name: 'ShareTechTube',
+        locale: 'ja_JP'
+      },
+      twitter: {
+        creator: '@',
+        card: 'summary_large_image'
+      }
+    }
+  end
 end
