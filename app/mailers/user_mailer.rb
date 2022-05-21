4b5cc813b2fab3e83e06f8a8c216511e98711844
kellyuka <yukamohri0719@yahoo.co.jp>
2022-05-21 09:22:53 +0900

    ADD:メイラーの定義追加

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+  default from: 'sharetechtube@gmail.com'
+  def reset_password_email
+    @user = User.find(user.id)
+    @url = edit_password_reset_url(@user.reset_password_token)
+    mail(to: user.email, subject: 'ShareTechTube】パスワード再設定')
+  end
+end
